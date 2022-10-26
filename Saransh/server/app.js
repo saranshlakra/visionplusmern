f154852085697d492b6198d76faaa2e227eaa8cb
Saransh Lakra <saranshlakra4@gmail.com>
2022-10-26 11:21:53 -0400

    Update app.js

--- Saransh/server/app.js
+++ Saransh/server/app.js
@@ -18,9 +18,9 @@ app.use(express.json());
 app.use(require('./router/auth'));
 const PORT = process.env.PORT || 5000; // port for heroku || port for local 
 
-app.get('/', (req, res) => {
-    res.send(`Hello world from the server`);
-})
+// app.get('/', (req, res) => { // for build use only
+//     res.send(`Hello world from the server`);
+// })
 // app.get('/TestPage', (req, res) => {
 //     res.cookie("test", "testToken");
 //     res.send(`Hello world from the about`);
